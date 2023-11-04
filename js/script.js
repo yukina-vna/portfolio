52ecbedccfabe0ee4e43e6cbb97717f44538a67f
yukina-vna <140179996+yukina-vna@users.noreply.github.com>
2023-11-04 14:58:45 +0900

    公開

---
+++ js/script.js
@@ -0,0 +1,93 @@
+
+  (function(d) {
+    var config = {
+      kitId: 'vjt1xyl',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+
+  (function(d) {
+    var config = {
+      kitId: 'vjt1xyl',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+
+  // ヘッダー
+  $(function () {
+    var $header = $("#header");
+    $(window).on("load scroll", function () {
+      var value = $(this).scrollTop();
+      if (value > 0) {
+        $header.addClass("scroll");
+      } else {
+        $header.removeClass("scroll");
+      }
+    });
+  });
+
+
+// ページネーション
+/* 
+this javascript is only to change the "actpage" attribut on the .cdp div
+*/
+
+window.onload = function(){
+  var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+  $('.cdp_i').on('click', function(){
+    var go = $(this).attr('href').replace('#!', '');
+    if (go === '+1') {
+      paginationPage++;
+    } else if (go === '-1') {
+      paginationPage--;
+    }else{
+      paginationPage = parseInt(go, 10);
+    }
+    $('.cdp').attr('actpage', paginationPage);
+  });
+};
+
+// ハンバーガーメニュー
+$(function () {
+
+  $('.sp_btn, .sp_nav li').on('click', function () {
+      $('.sp_btn').toggleClass('open');
+      $('.sp_nav').fadeToggle();
+  });
+});
+
+// ヘッダー カレント表示
+const headerNavLink = document.querySelectorAll('.js-header');
+
+headerNavLink.forEach((targetLink) => {
+  if (targetLink.href === location.href) {
+    targetLink.parentElement.classList.add('is-current');
+  }
+});
+
+// ローディング
+$(window).on("load", function () {
+  // ローディング画面を非表示にする
+  $(".loader").delay(1700).fadeOut(650);
+
+  // 2秒後に真っ暗な背景をフェードイン
+  setTimeout(function () {
+      $(".overlay").css("visibility", "visible");
+      $(".overlay").css("opacity", "1"); // 不透明にすることでフェードイン
+  }, 2000); // 2秒後にフェードイン
+
+  // 3秒後に真っ暗な背景をフェードアウト
+  setTimeout(function () {
+      $(".overlay").css("opacity", "0"); // 透明にすることでフェードアウト
+  }, 3000); // 3秒後にフェードアウト
+
+  // 4秒後にコンテンツが表示される
+  setTimeout(function () {
+      $(".content").css("visibility", "visible");
+      $(".content").css("opacity", "1"); // 不透明にすることでフェードイン
+  }, 4000); // 4秒後にコンテンツを表示
+});
---
+++ works/js/modal.js
@@ -0,0 +1,102 @@
+
+(function(d) {
+  var config = {
+    kitId: 'vjt1xyl',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
+(function(d) {
+  var config = {
+    kitId: 'vjt1xyl',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
+// works詳細ページ view all
+ const buttonOpen = document.getElementById('modalOpen');
+ const modal = document.getElementById('easyModal');
+ const buttonClose = document.getElementsByClassName('modalClose')[0];
+ 
+ // ボタンがクリックされた時
+ buttonOpen.addEventListener('click', modalOpen);
+ function modalOpen() {
+   modal.style.display = 'block';
+ }
+ 
+ // バツ印がクリックされた時
+ buttonClose.addEventListener('click', modalClose);
+ function modalClose() {
+   modal.style.display = 'none';
+ }
+ 
+ // モーダルコンテンツ以外がクリックされた時
+ addEventListener('click', outsideClose);
+ function outsideClose(e) {
+   if (e.target == modal) {
+     modal.style.display = 'none';
+   }
+ }
+ 
+ // SP
+ const buttonOpen_sp1 = document.getElementById('modalOpen_sp1');
+ const modal_sp1 = document.getElementById('easyModal_sp1');
+ const buttonClose_sp1 = document.getElementsByClassName('modalClose_sp')[0];
+ 
+ const buttonOpen_sp2 = document.getElementById('modalOpen_sp2');
+ const modal_sp2 = document.getElementById('easyModal_sp2');
+ const buttonClose_sp2 = document.getElementsByClassName('modalClose_sp')[1];
+ 
+ // ボタンがクリックされた時
+ buttonOpen_sp1.addEventListener('click', modalOpen_sp1);
+ function modalOpen_sp1() {
+   modal_sp1.style.display = 'block';
+ }
+ 
+ // バツ印がクリックされた時
+ buttonClose_sp1.addEventListener('click', modalClose_sp);
+ buttonClose_sp2.addEventListener('click', modalClose_sp);
+ function modalClose_sp() {
+   modal_sp1.style.display = 'none';
+   modal_sp2.style.display = 'none';
+ }
+ 
+ // モーダルコンテンツ以外がクリックされた時
+ addEventListener('click', outsideClose);
+ function outsideClose(e) {
+   if (e.target == modal) {
+     modal_sp1.style.display = 'none';
+   }
+ }
+ 
+ // ボタンがクリックされた時
+ buttonOpen_sp2.addEventListener('click', modalOpen_sp2);
+ function modalOpen_sp2() {
+   modal_sp2.style.display = 'block';
+ }
+ 
+ // モーダルコンテンツ以外がクリックされた時
+ addEventListener('click', outsideClose);
+ function outsideClose(e) {
+   if (e.target == modal) {
+     modal_sp2.style.display = 'none';
+   }
+ }
+
+ // ヘッダー
+ $(function () {
+    var $header = $("#header");
+    $(window).on("load scroll", function () {
+      var value = $(this).scrollTop();
+      if (value > 0) {
+        $header.addClass("scroll");
+      } else {
+        $header.removeClass("scroll");
+      }
+    });
+  });
+ 
